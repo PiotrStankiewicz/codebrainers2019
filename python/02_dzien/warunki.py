c48ca20c36ed6768aa2ccd5e7df6907468af331a
Piotr Stankiewicz <stankiewiczp89@gmail.com>
2019-02-25 20:31:24 +0100

    python_2dzien

---
+++ python/02_dzien/warunki.py
@@ -0,0 +1,14 @@
+zmienna1 = 123
+zmienna2 = 33.33
+zmienna3 = 'Piotr'
+zmienna4 = True
+zmienna5 = None
+zmienna6 = [1,2,3,4,5]
+
+print(zmienna1)
+print(zmienna2)
+print(zmienna3)
+print(zmienna4)
+print(zmienna5)
+print(zmienna6)
+
