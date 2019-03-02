a8d140441d9b092fee110d802593c3860a989332
Piotr Stankiewicz <stankiewiczp89@gmail.com>
2019-03-02 10:18:54 +0100

    4 dzien

---
+++ dni/02_dzien/warunki.py
@@ -0,0 +1,41 @@
+zmienna1 = 123
+zmienna2 = 33.33
+zmienna3 = 'Piotr'
+zmienna4 = True
+zmienna5 = None
+zmienna6 = [1,2,3,4,5]
+
+print(zmienna1)
+print(zmienna2)
+print(zmienna3)
+print(zmienna4)
+print(zmienna5)
+print(zmienna6)
+
+# komentarz 
+# PEP8
+
+#instrukcje warunkowe   if
+#jesli jakis warunek jest spelniony to wtedy sa wywolywane instrukcje jak nie to inne 
+if WARUNEK:
+    # instrukcje jesli WARUNEK jest spelniony
+    pass
+elif INNY WARUNEK:
+    #instrukcje jesli INNY WARUNEK jest spelniony
+    pass
+else:
+    #instrukccje jesli zaden z powyzszych nie jest spelniony
+    pass
+
+if zmienna1 == 3:
+    print(3)
+if zmienna1 == 4:
+    print(4)
+if zmienna1 == 5:
+    print(5)    
+elif zmienna1 == "string":
+    print("string")
+else
+    print('nie miales racji')
+
+    #cwiczenia algorytmiczne na nastepne zajecia
---
+++ dni/04_dzien/petle.py
@@ -0,0 +1,35 @@
+uczestnik = 'Piotr'
+
+dziennik = ['Piotr','Adam','Jozek','Tomek','Konrad', 'Piotr']
+
+# if 'Piotr' 
+#  print('Piotr')
+#  else ('To nie jest Piotr')
+
+
+
+
+#  for zmienna in dziennik 
+    # pass
+
+
+# for piotr in lista_imion:
+    
+    # czym sie rozni pass od print?
+    #   if piotr == piotr
+    # print('Piotr')
+    # else('To nie jest Piotr')
+
+    
+for uczen in dziennik:
+    if uczen == uczestnik:
+        print("znalazlem cie:", uczen)
+    else:
+        print('nie znalazlem')
+
+
+for pozycja, uczen in enumerate(dziennik):
+#     print(pozycja, “: “, uczen)
+    if uczen == uczestnik:
+        print('Piotr', pozycja)
+    
